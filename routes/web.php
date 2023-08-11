bc91bec1273bced06ae482755123f1d4d13b87c0
averiechristy2101 <averie.422020006@civitas.ukrida.ac.id>
2023-08-11 15:35:05 +0700

    slicing admin

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,58 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/login', function () {
+    return view('login');
+});
+
+Route::get('/admin/dashboard', function () {
+    return view('admin/dashboard');
+});
+
+Route::get('/admin/userrole/index', function () {
+    return view('/admin/userrole/index');
+});
+
+Route::get('/admin/userrole/create', function () {
+    return view('/admin/userrole/create');
+});
+
+
+Route::get('/admin/useraccount/index', function () {
+    return view('/admin/useraccount/index');
+});
+
+Route::get('/admin/useraccount/create', function () {
+    return view('/admin/useraccount/create');
+});
+
+
+Route::get('/admin/product/index', function () {
+    return view('/admin/product/index');
+});
+
+Route::get('/admin/product/create', function () {
+    return view('/admin/product/create');
+});
+
+Route::get('/admin/package/index', function () {
+    return view('/admin/package/index');
+});
+
+Route::get('/admin/package/create', function () {
+    return view('/admin/package/create');
+});
+
+
+Route::get('/admin/artikel/index', function () {
+    return view('/admin/artikel/index');
+});
+
+Route::get('/admin/artikel/create', function () {
+    return view('/admin/artikel/');
+});
+
+Route::get('/admin/changepassword', function () {
+    return view('/admin/changepassword');
+});
